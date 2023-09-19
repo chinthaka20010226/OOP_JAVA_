ebe28782d7afc3bdbccce3c4d730c0826153a4db
chinthaka20010226 <prabathchinthaka361@gmail.com>
2023-09-19 11:23:57 +0530

    static

---
+++ working_area/static_/static_.java
@@ -0,0 +1,29 @@
+// ---> 01) static variable(class variable / template variable)
+
+class MyClass{
+    int a;
+  
+    static int x;
+}
+
+class Demo{
+    public static void main(String args[]){
+         MyClass c1=new MyClass();
+         c1.a=1;
+         c1.x=2;
+
+         MyClass c2=new MyClass();
+         c2.a=10;
+         c2.x=20;
+
+         MyClass c3=new MyClass();
+         c3.a=100;
+         c3.x=200;
+
+         MyClass.x=0;
+
+         System.out.println("c1 : "+c1.a+" "+c1.x);
+         System.out.println("c2 : "+c2.a+" "+c2.x);
+         System.out.println("c3 : "+c3.a+" "+c3.x);
+    }
+}
