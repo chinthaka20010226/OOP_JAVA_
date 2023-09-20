3daee945556549480d1a9c271658fa2dc4f01541
chinthaka20010226 <prabathchinthaka361@gmail.com>
2023-09-20 13:12:22 +0530

    diagram of object reference variable & object reference variable as instance variable of a class

---
+++ working_area/as_data_structures_&_algorithms/think_.java
@@ -0,0 +1,68 @@
+
+/*class Student{
+    int code;
+    String name;
+  
+}
+
+class Demo{
+    public static void main(String args[]){
+         Student s1;
+         System.out.println(s1);
+    }
+}*/
+
+
+// ====================================================================================================
+
+/*class StudentMarks{
+    int dbms;
+    int prf;
+}
+
+class Student{
+    int code;
+    String name;
+    StudentMarks marks;
+
+    Student(int code,String name){
+       this.code=code;
+       this.name=name;
+    }
+}
+
+class Demo{
+    public static void main(String args[]){
+         Student s1=new Student(214189,"Danapala");
+
+         System.out.println("code : "+s1.code);
+         System.out.println("name : "+s1.name);
+         System.out.println("marks : "+s1.marks);
+    }
+}*/
+
+
+// ===============================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
